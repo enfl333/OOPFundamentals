e6e26fabebeea34c613141efba175fbfa51e3dbc
enfl333 <enrique.pflores@gmail.com>
2021-02-08 21:52:43 +0100

    An Interface and it's implementation class

---
+++ src/main/java/com/flores/oop/ImplementInterface.java
@@ -0,0 +1,9 @@
+package com.flores.oop;
+
+public class ImplementInterface implements MyInterface{
+
+    @Override
+    public void myImplementationOfTruth() {
+        System.out.println("Implement interface");
+    }
+}
---
+++ src/main/java/com/flores/oop/MyInterface.java
@@ -0,0 +1,6 @@
+package com.flores.oop;
+
+public interface MyInterface {
+
+    public void myImplementationOfTruth();
+}
