c494b13cf073f9a8bc8da9f4351913efed715313
enfl333 <enrique.pflores@gmail.com>
2021-02-08 21:57:03 +0100

    A new  Interface and it's new implementation class

---
+++ src/main/java/com/flores/oop/NewImplementInterfaceClass.java
@@ -0,0 +1,8 @@
+package com.flores.oop;
+
+public class NewImplementInterfaceClass implements MyInterface{
+    @Override
+    public void myImplementationOfTruth() {
+        System.out.println("New Class implementing Interface");
+    }
+}
